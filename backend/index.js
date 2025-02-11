e38430c965ed43e43036da12aafe7fbc8bcdff85
maheeen-khan <maheeenkhan4@gmail.com>
2025-02-11 22:06:13 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,46 @@
+import 'dotenv/config'
 import express from 'express'
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 4000
+
+const stdData = [
+    {
+        name: "Maheen",
+        rollNo: 26,
+        description: "She is doing software engineering from University of Karachi."
+    },
+    {
+        name: "Ashna",
+        rollNo: 28,
+        description: "She is very intelligent girl in Class 10th."
+    },
+    {
+        name: "Sarah",
+        rollNo: 27,
+        description: "Passionate about science and loves experiments."
+    },
+    {
+        name: "Olivia",
+        rollNo: 29,
+        description: "A bookworm who loves literature and poetry."
+    },
+    {
+        name: "Daniel",
+        rollNo: 31,
+        description: "Enjoys coding and participates in hackathons."
+    },
+    {
+        name: "John",
+        rollNo: 33,
+        description: "A diligent student who excels in mathematics."
+    }
+]
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(stdData)
     })
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
