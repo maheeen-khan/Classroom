185b861e8fde05b1753cfedb38ae3a20def4d4ed
maheeen-khan <maheeenkhan4@gmail.com>
2025-03-25 22:40:07 +0500

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import connectToDB from './db/db.mjs'
+
+connectToDB()
+
 const app = express()
 const port = process.env.PORT || 4000
 
